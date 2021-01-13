15153541c87722a14d10ef6ab4423e1d997146f8
prodigysurge <boatinglife6@yahoo.com>
2021-01-13 18:58:13 -0500

    commit tree 6ceb7e37020d0f0eb6462f7b9d1becaba7018db8

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+  end
+  
+  def batch_badge_creator(array)
+      array.collect {|x| badge_maker(x)}
+  end
+  
+  def assign_rooms(array)
+  
+    array.each_with_index { |value,index| array[index] = "Hello, #{value}! You'll be assigned to room #{index + 1}!"}
+  
+    return array
+  end
+  
+  def printer(array)
+  
+  
+    batch_badge_creator(array).each do |value| 
+        puts value 
+    end
+    assign_rooms(array).each do |value| 
+        puts value 
+    end
+
+  end
